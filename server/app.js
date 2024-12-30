0dc27f215274b2503cb63fad18144dfdea963ac2
nimeresam <nimeresam95@gmail.com>
2024-12-30 09:14:23 +0300

    feat: add server

--- server/app.js
+++ server/app.js
@@ -4,9 +4,17 @@ const app = express();
 // Middleware
 app.use(express.json());
 
-// Test route
-app.get("/", (req, res) => {
-  res.send("Hello from the backend!");
+// Serve static files from React
+app.use(express.static(path.join(__dirname, "../client/build")));
+
+// API route example
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from the backend!" });
+});
+
+// Serve React app for other routes
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
 // Start server
