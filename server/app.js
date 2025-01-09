1fe0e08713af599b21bade5b2347f82ba8504e9f
nimeresam <nimeresam95@gmail.com>
2025-01-09 10:48:35 +0300

    feat: add cors

--- server/app.js
+++ server/app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const path = require("path");
 const multer = require("multer");
+const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
 
 // Middlewares
+app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "../client/build")));
 app.use((err, req, res, next) => {
